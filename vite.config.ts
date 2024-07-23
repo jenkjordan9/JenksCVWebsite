e2c498ecb3c35b1b657dd12b0a2a6d476138ed2e
jenkjordan9 <jenk.ali9@outlook.com>
2024-07-23 23:07:46 +0100

    deploy changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // or 'dist', depending on your preference
-  },
-  base: '/JenksCVWebsite/', // This should match the repository name if using a GitHub Pages project site
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/JenksCVWebsite/' : '/', // Adjust this to match your repository name for GitHub Pages
+    build: {
+      outDir: 'build', // Ensure this matches your deployment directory
+    },
+  };
 });
