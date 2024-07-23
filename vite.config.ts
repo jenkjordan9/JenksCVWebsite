2e2274a86356d91c48fb7bc92f60c9f666d18bdb
jenkjordan9 <jenk.ali9@outlook.com>
2024-07-23 23:42:20 +0100

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === 'production' ? '/JenksCVWebsite/' : '/', // Adjust this to match your repository name for GitHub Pages
+    base: mode === 'production' ? '/JenksCVWebsite/' : '/',
     build: {
-      outDir: 'build', // Ensure this matches your deployment directory
+      outDir: 'build',
+      assetsDir: 'assets',
     },
   };
 });
