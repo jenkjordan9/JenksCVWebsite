1584055c0ce2f5eb644a05630f9edebe35c0657c
jenkjordan9 <jenk.ali9@outlook.com>
2024-07-23 22:13:20 +0100

    package update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // or 'dist', depending on your preference
+  },
+  base: '/JenksCVWebsite/', // This should match the repository name if using a GitHub Pages project site
+});
